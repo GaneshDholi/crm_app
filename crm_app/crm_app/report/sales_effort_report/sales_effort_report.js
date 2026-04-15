99f00ca24ca3ca337f2e7c1613a59f560fbe15b4
GaneshDholi <ganeshdholi88@gmial.com>
2026-04-15 12:55:20 +0000

    report

--- crm_app/crm_app/report/sales_effort_report/sales_effort_report.js
+++ crm_app/crm_app/report/sales_effort_report/sales_effort_report.js
@@ -1,15 +1,23 @@
-filters: [
-    {
-        fieldname: "activity_user",
-        label: "User",
-        fieldtype: "Link",
-        options: "User"
-    },
-    {
-        fieldname: "date",
-        label: "Date",
-        fieldtype: "Select",
-        options: ["Today", "Yesterday", "Last 7 Days"],
-        default: "Today"
-    }
-]
+frappe.query_reports["Sales Effort Report"] = {
+    filters: [
+        {
+            fieldname: "activity_user",
+            label: "User",
+            fieldtype: "Link",
+            options: "User",
+            width: 200
+        },
+        {
+            fieldname: "date",
+            label: "Date",
+            fieldtype: "Select",
+            options: [
+                "Today",
+                "Yesterday",
+                "Last 7 Days"
+            ],
+            default: "Today",
+            width: 150
+        }
+    ]
+};
